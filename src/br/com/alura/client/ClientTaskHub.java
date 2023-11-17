aab4addaf61088c362d8ce554af44aed427b459e
camilazci <camilazucchi@outlook.com>
2023-11-17 14:15:29 -0300

    feat: add client class

---
+++ src/br/com/alura/client/ClientTaskHub.java
@@ -0,0 +1,11 @@
+package br.com.alura.client;
+
+import java.net.Socket;
+
+public class ClientTaskHub {
+    public static void main(String[] args) throws Exception {
+
+        Socket socket = new Socket("localhost", 12345);
+
+    }
+}
