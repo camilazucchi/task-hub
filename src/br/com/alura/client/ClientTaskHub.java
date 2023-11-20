7044acc518058324c2e5236299ff963217e6e7d9
camilazci <camilazucchi@outlook.com>
2023-11-20 15:00:42 -0300

    feat: add printstream to simulate a command process

--- src/br/com/alura/client/ClientTaskHub.java
+++ src/br/com/alura/client/ClientTaskHub.java
@@ -1,14 +1,26 @@
 package br.com.alura.client;
 
+import java.io.PrintStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ClientTaskHub {
     public static void main(String[] args) throws Exception {
+        Socket socket;
 
-        Socket socket = new Socket("localhost", 12345);
+        socket = new Socket("localhost", 12345);
 
         System.out.println("Conexão estabelecida com sucesso!");
 
+        PrintStream stream = new PrintStream(socket.getOutputStream());
+        stream.println("C1");
+
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.nextLine();
+
+        stream.close();
+        scanner.close();
         socket.close();
 
     }
