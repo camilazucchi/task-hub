eecffb41016a530ea8f7ce8128871c563ce9d635
camilazci <camilazucchi@outlook.com>
2023-11-22 15:55:35 -0300

    feat: extract start and stop methods, add constructor to server class

--- src/br/com/alura/server/TaskDistributor.java
+++ src/br/com/alura/server/TaskDistributor.java
@@ -1,14 +1,17 @@
 package br.com.alura.server;
 
+import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class TaskDistributor implements Runnable {
     private static final Object lock = new Object();
     private Socket socket;
+    private TaskHub server;
 
-    public TaskDistributor(Socket socket) {
+    public TaskDistributor(Socket socket, TaskHub server) {
         this.socket = socket;
+        this.server = server;
     }
 
     @Override
@@ -20,13 +23,35 @@ public class TaskDistributor implements Runnable {
             synchronized (lock) {
                 // Recebe a entrada através do método ".getInputStream()":
                 Scanner clientInput = new Scanner(socket.getInputStream());
+                PrintStream clientOutput = new PrintStream(socket.getOutputStream());
 
                 // Entra em um loop enquanto houver novas linhas:
                 while (clientInput.hasNextLine()) {
                     String command = clientInput.nextLine();
+                    System.out.println("Comando recebido: " + command);
+
+                    switch (command) {
+                        case "c1":
+                            clientOutput.println("O comando C1 foi enviado!");
+                            clientOutput.flush();
+                            break;
+                        case "c2":
+                            clientOutput.println("O comando C2 foi enviado!");
+                            clientOutput.flush();
+                            break;
+                        case "fim":
+                            clientOutput.println("Encerrando o servidor...");
+                            server.stopServer();
+                            break;
+                        default:
+                            clientOutput.println("Comando não encontrado.");
+                            clientOutput.flush();
+                    }
+
                     System.out.println(command);
                 }
 
+                clientOutput.close();
                 clientInput.close();
             }
         } catch (Exception exception) {
