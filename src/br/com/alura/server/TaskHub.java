867c4987d9e8252af9936207b90f1c2d500557f5
camilazci <camilazucchi@outlook.com>
2023-11-27 13:48:38 -0300

    feat: add AtomicBoolean to isRunning attribute

--- src/br/com/alura/server/TaskHub.java
+++ src/br/com/alura/server/TaskHub.java
@@ -6,26 +6,27 @@ import java.net.Socket;
 import java.net.SocketException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class TaskHub {
     private final ServerSocket serverSocket;
     private final ExecutorService executorService;
-    private boolean isRunning;
+    private AtomicBoolean isRunning;
 
     public TaskHub() throws IOException {
         System.out.println("Servidor esperando por conexões...");
         this.serverSocket = new ServerSocket(12345);
         this.executorService = Executors.newCachedThreadPool();
-        this.isRunning = true;
+        this.isRunning = new AtomicBoolean(true);
     }
 
     // Método para iniciar o servidor:
     public void startServer() throws IOException {
-        while (this.isRunning) {
+        while (this.isRunning.get()) {
             try {
                 Socket socket = serverSocket.accept();
                 System.out.println("Aceitando novo cliente na porta: " + socket.getPort());
-                TaskDistributor taskDistributor = new TaskDistributor(socket, this);
+                TaskDistributor taskDistributor = new TaskDistributor(executorService, socket, this);
                 executorService.execute(taskDistributor);
             } catch (SocketException e) {
                 System.out.println("SocketException está rodando? " + this.isRunning);
@@ -37,7 +38,7 @@ public class TaskHub {
     /* Obs.: o servidor não para enquanto tiver um cliente conectado, somente quando o cliente é desconectado
     que o servidor para de rodar. */
     public void stopServer() throws IOException {
-        this.isRunning = false;
+        this.isRunning.set(false);
         serverSocket.close();
         executorService.shutdown();
     }
