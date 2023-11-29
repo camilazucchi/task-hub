69a771cac96d332a5a4cd10594f15d9c70a86699
camilazci <camilazucchi@outlook.com>
2023-11-29 13:56:37 -0300

    refactor: refactor C2 web service caller

--- src/br/com/alura/server/C2WebServiceCaller.java
+++ src/br/com/alura/server/C2WebServiceCaller.java
@@ -1,26 +1,30 @@
 package br.com.alura.server;
 
 import java.io.PrintStream;
+import java.util.Random;
+import java.util.concurrent.Callable;
 
-public class C2WebServiceCaller implements Runnable {
+public class C2WebServiceCaller implements Callable<String> {
 
     private final PrintStream output;
+    private final Random random = new Random();
 
     public C2WebServiceCaller(PrintStream output) {
         this.output = output;
     }
 
     @Override
-    public void run() {
-        System.out.println("Executando o comando C2.");
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        throw new RuntimeException("Exception no comando C2...");
-
-        //output.println("Comando C2 executado com sucesso!");
+    public String call() throws Exception {
+        // Saída no console do SERVIDOR:
+        System.out.println("Servidor recebeu o comando C2 - WebService.");
+        // Saída para o CLIENTE:
+        output.println("Processando o comando C2 - WebService.");
+        // Aguarda 20 segundos:
+        Thread.sleep(25000);
+        // Gera um número aleatório:
+        int number = random.nextInt(100) + 1;
+        // Saída no console do SERVIDOR:
+        System.out.println("Servidor finalizou a execução do comando C2 - WebService.");
+        return Integer.toString(number);
     }
 }
